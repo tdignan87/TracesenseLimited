03b537046014b8fb8e5a765ca7448e1ae6cc6be9
Tom <tdignan87@gmail.com>
2020-04-12 19:59:11 +0100

    Updated

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    $("#aboutUs").hide();
+}
+
+$("#navAboutUs").click(function() {;
+    $("#aboutUs").show();
+})
+
+$("navAboutUs").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("aboutUs");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
